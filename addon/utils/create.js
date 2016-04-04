2f528118bbd5dcc4b7ef6d1c687857bbb4bbad47
hvostya <tosotova@ics.perm.ru>
2016-04-04 16:47:05 +0500

    Add projection name to model projection object

--- addon/utils/create.js
+++ addon/utils/create.js
@@ -9,12 +9,14 @@
  * @for Projection
  * @public
  *
+ * @param {String} projectionName The name of projection.
  * @param {String} modelName The name of the model type.
  * @param {Object} attributes Projection attributes.
  * @return {Object} Model projection.
  */
-export default function create(modelName, attributes) {
+export default function create(projectionName, modelName, attributes) {
   return {
+    projectionName: projectionName,
     modelName: modelName,
     attributes: attributes || {}
   };
