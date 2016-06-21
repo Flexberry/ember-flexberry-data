4350d39964de388f4855423b38038de13aa30dfe
Eugene Kolegov <ekolegov@outlook.com>
2016-06-21 16:22:41 +0500

    fix order-by-clause master support

--- addon/query/order-by-clause.js
+++ addon/query/order-by-clause.js
@@ -13,22 +13,14 @@ export default class OrderByClause {
    */
   constructor(clause) {
     this._clause = [];
-    this._masterClause = [];
     clause.split(',').forEach((i) => {
       let s = i.trim().split(' ');
       let n = s[0].trim().split('.');
-      if (n.length > 1) {
-        this._masterClause[n[0].trim()] = this._masterClause[n[0].trim()] ? this._masterClause[n[0].trim()] : [];
-        this._masterClause[n[0].trim()].push({
-          name: n[1],
-          direction: s[1],
-        });
-      } else {
-        this._clause.push({
-          name: s[0],
-          direction: s[1],
-        });
-      }
+      this._clause.push({
+        name: n.length > 1 ? n[1] : s[0],
+        master: n.length > 1 ? n[0] : false,
+        direction: s[1],
+      });
     });
   }
 
@@ -52,32 +44,5 @@ export default class OrderByClause {
    */
   attribute(index) {
     return this._clause[index];
-  }
-
-  /**
-    Returns count elements for sorting.
-
-    @method masterLength
-    @param {String} master Name master object.
-    @return {Number} Count elements for sorting.
-   */
-  masterLength(master) {
-    if (this._masterClause[master]) {
-      return this._masterClause[master].length;
-    }
-
-    return 0;
-  }
-
-  /**
-    Returns object with parameters for sorting, for specified master object.
-
-    @method masterAttribute
-    @param {String} master Name master object.
-    @param {Number} index Index element in array.
-    @return {Object} Object with parameters for sorting.
-   */
-  masterAttribute(master, index) {
-    return this._masterClause[master][index];
   }
 }
