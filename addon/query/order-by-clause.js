666353337a8899bf6176847f634e56c6df074ce2
Eugene Kolegov <ekolegov@outlook.com>
2016-06-27 19:48:56 +0500

    fix order-by-clause error with empty string

--- addon/query/order-by-clause.js
+++ addon/query/order-by-clause.js
@@ -13,19 +13,21 @@ export default class OrderByClause {
    */
   constructor(clause) {
     this._clause = [];
-    clause.split(',').forEach((i) => {
-      let s = i.trim().split(' ');
-      let name;
-      let direction = s[s.length - 1];
-      if (direction === 'asc' || direction === 'desc') {
-        name = s.slice(0, s.length - 1).join(' ');
-      } else {
-        name = s.slice(0, s.length).join(' ');
-        direction = '';
-      }
+    if (clause) {
+      clause.split(',').forEach((i) => {
+        let s = i.trim().split(' ');
+        let name;
+        let direction = s[s.length - 1];
+        if (direction === 'asc' || direction === 'desc') {
+          name = s.slice(0, s.length - 1).join(' ');
+        } else {
+          name = s.slice(0, s.length).join(' ');
+          direction = '';
+        }
 
-      this._clause.push({ name, direction });
-    });
+        this._clause.push({ name, direction });
+      });
+    }
   }
 
   /**
