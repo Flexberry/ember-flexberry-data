b13cf78f31a4f8ba88414affa0cf3e21298f2a8b
Alexander Elyseev <alexander.elyseev@gmail.com>
2016-07-01 18:56:18 +0500

    Add mock test for creating entity using OData

--- tests/unit/query/odata-adapter-integrated-test.js
+++ tests/unit/query/odata-adapter-integrated-test.js
@@ -8,7 +8,8 @@ import startApp from '../../helpers/start-app';
 import config from '../../../../dummy/config/environment';
 
 if (config.APP.testODataService) {
-  const baseUrl = 'http://flexberry-ember-dummy.azurewebsites.net/odata';
+  const randKey = Math.floor(Math.random() * 999);
+  const baseUrl = 'http://rtc-web:8081/odatatmp/ember' + randKey;
   const app = startApp();
   const store = app.__container__.lookup('service:store');
 
@@ -26,9 +27,18 @@ if (config.APP.testODataService) {
   test('adapter | integrated odata | without predicate', (assert) => {
     let builder = new QueryBuilder(store, 'ember-flexberry-dummy-suggestion');
 
-    console.log();
     runTest(assert, store, builder, (data) => {
       assert.ok(data);
+    });
+  });
+
+  test('adapter | integrated odata | create', (assert) => {
+    assert.expect(0);
+    let done = assert.async();
+    Ember.run(() => {
+      store.createRecord('ember-flexberry-dummy-localization', {
+        name: 'Test'
+      }).save().then(done);
     });
   });
 }
