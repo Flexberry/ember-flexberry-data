f17bfe8f9a4677f26c896780bb73e9ee625c362f
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-07-19 20:01:58 +0500

    Fix `release.js`

--- config/release.js
+++ config/release.js
@@ -1,8 +1,30 @@
 /* jshint node: true */
 
+"use strict";
+
 // var RSVP = require('rsvp');
 var path = require('path');
 var fs = require('fs');
+
+function updateVersion(project, versions, fileWithVersionPath) {
+  let filePath = path.join(project.root, fileWithVersionPath);
+  if (fs.existsSync(filePath)) {
+    let contents = fs.readFileSync(filePath, {
+      encoding: 'utf8'
+    });
+     // Version string without 'v' tag prefix.
+    let newVersion = versions.next.replace(/^v/, '');
+
+    // Replace version number in line "var version = 'x.x.x';".
+    contents = contents.replace(/(version = ')(.+)(';)/, '$1' + newVersion + '$3');
+
+    fs.writeFileSync(filePath, contents, {
+      encoding: 'utf8'
+    });
+  } else {
+    throw new Error('File not found: ' + filePath);
+  }
+}
 
 // For details on each option run `ember help release`
 module.exports = {
@@ -23,28 +45,7 @@ module.exports = {
 
   // TODO: rewrite using promises and async fs.
   beforeCommit: function(project, versions) {
-    let updateVersion = (fileWithVersionPath) => {
-      let filePath = path.join(project.root, fileWithVersionPath);
-      if (fs.existsSync(filePath)) {
-        let contents = fs.readFileSync(filePath, {
-          encoding: 'utf8'
-        });
-
-        // Version string without 'v' tag prefix.
-        let newVersion = versions.next.replace(/^v/, '');
-
-        // Replace version number in line "var version = 'x.x.x';".
-        contents = contents.replace(/(version = ')(.+)(';)/, '$1' + newVersion + '$3');
-
-        fs.writeFileSync(filePath, contents, {
-          encoding: 'utf8'
-        });
-      } else {
-        throw new Error('File not found: ' + filePath);
-      }
-    };
-
-    updateVersion('vendor/ember-flexberry-data/register-version.js');
-    updateVersion('addon/version.js');
+    updateVersion(project, versions, 'vendor/ember-flexberry-data/register-version.js');
+    updateVersion(project, versions, 'addon/version.js');
   }
 };
