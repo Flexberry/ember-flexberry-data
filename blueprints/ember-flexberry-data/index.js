14d89ef057c3b42657a4c4f81d90a1c9262a8444
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-07-25 17:33:04 +0500

    Cleanup default bluprint of junk

--- blueprints/ember-flexberry-data/index.js
+++ blueprints/ember-flexberry-data/index.js
@@ -1,27 +1,18 @@
 /* globals module */
 module.exports = {
-  description: 'Adds necessary packages to application',
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  afterInstall: function(options) {
-    var _this = this;
+  afterInstall: function() {
+    var _self = this;
     return this.addBowerPackagesToProject([
       { name: 'localforage', target: '1.3.3' }
     ]).then(function() {
-      return _this.addAddonsToProject({
+      return _self.addAddonsToProject({
         packages: [
           'https://github.com/Flexberry/ember-localforage-adapter.git',
           { name: 'ember-browserify', target: '1.1.9' }
         ]
       });
     }).then(function () {
-      return _this.addPackagesToProject([
+      return _self.addPackagesToProject([
         { name: 'dexie', target: '1.3.6' }
       ]);
     });
