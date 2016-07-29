b0811b14686f7baef7243c44f9063da7be7c5399
DubrovinPavel <dubrovip@yandex.ru>
2016-07-29 15:27:46 +0500

    Add test for building OData query with cyrillic attribute names

---
+++ tests/dummy/app/models/cyrillic.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+export default Model.extend({
+  имя: attr('string'),
+  name: attr('string')
+});
