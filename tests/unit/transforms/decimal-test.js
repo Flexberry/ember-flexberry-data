c7152440f53183de39185dafb90fd7de40c3cc85
DrKaotica <user.email>
2016-08-08 14:04:18 +0500

    Created new transformation 'decimal' and tests for it

---
+++ tests/unit/transforms/decimal-test.js
@@ -0,0 +1,34 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('transform:decimal', 'Unit | Transform | decimal', {
+});
+
+test('decimal | deserialize | number', function (assert) {
+  let transform = this.subject();
+  let deserialized = transform.deserialize(555.5);
+  assert.equal(deserialized, 555.5);
+});
+
+test('decimal | deserialize | string', function (assert) {
+  let transform = this.subject();
+  let deserialized = transform.deserialize('555.5');
+  assert.equal(deserialized, 555.5);
+});
+
+test('decimal | serialize | number', function (assert) {
+  let transform = this.subject();
+  let serialized = transform.serialize(555.5);
+  assert.equal(serialized, 555.5);
+});
+
+test('decimal | serialize | string with \'.\'', function (assert) {
+  let transform = this.subject();
+  let serialized = transform.serialize('555.5');
+  assert.equal(serialized, 555.5);
+});
+
+test('decimal | serialize | string with \',\'', function (assert) {
+  let transform = this.subject();
+  let serialized = transform.serialize('555,5');
+  assert.equal(serialized, 555.5);
+});
