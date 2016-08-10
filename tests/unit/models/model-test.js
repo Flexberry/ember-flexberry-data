9c3e1a2c95f2520716c92931a1801a8d5204201c
Eugene Kolegov <ekolegov@outlook.com>
2016-08-10 12:44:41 +0500

    Test model divide and rule

--- tests/unit/models/model-test.js
+++ tests/unit/models/model-test.js
@@ -21,83 +21,64 @@ test('it exists', function(assert) {
   assert.ok(!!model);
 });
 
-test('rollback relationships', function(assert) {
+test('rollback hasMany relationships', function(assert) {
   Ember.run(() => {
     let store = this.store();
 
-    let type1 = store.createRecord('ember-flexberry-dummy-suggestion-type', {
-      name: 'type1',
-      moderated: false,
-    });
-    let type2 = store.createRecord('ember-flexberry-dummy-suggestion-type', {
-      name: 'type2',
-      moderated: true,
-    });
+    let suggestion = store.createRecord('ember-flexberry-dummy-suggestion');
 
-    let user1 = store.createRecord('ember-flexberry-dummy-application-user', {
-      name: 'user1',
-      eMail: 'eMail',
-      phone1: 'phone1',
-      phone2: 'phone2',
-      phone3: 'phone3',
-      activated: false,
-      vK: 'vK',
-      facebook: 'facebook',
-      twitter: 'twitter',
-      birthday: new Date(),
-      vip: false,
-      karma: 0,
-    });
-    let user2 = store.createRecord('ember-flexberry-dummy-application-user', {
-      name: 'user2',
-      eMail: 'eMail',
-      phone1: 'phone1',
-      phone2: 'phone2',
-      phone3: 'phone3',
-      activated: false,
-      vK: 'vK',
-      facebook: 'facebook',
-      twitter: 'twitter',
-      birthday: new Date(),
-      vip: false,
-      karma: 0,
-    });
+    let vote1 = store.createRecord('ember-flexberry-dummy-vote');
+    let vote2 = store.createRecord('ember-flexberry-dummy-vote');
+
+    let comment1 = store.createRecord('ember-flexberry-dummy-comment');
+    let comment2 = store.createRecord('ember-flexberry-dummy-comment');
+
+    //Change `hasMany` relationships.
+    suggestion.set('userVotes', [vote1, vote2]);
+    suggestion.set('comments', [comment1, comment2]);
+
+    //Diff `hasMany` relationships.
+    assert.deepEqual(suggestion.changedHasMany(), {
+      userVotes: [[], [vote1, vote2]],
+      comments: [[], [comment1, comment2]],
+    }, `Results 'changedHasMany' function as expected.`);
+
+    //Rollback `hasMany` for only `userVotes` relationship.
+    suggestion.rollbackHasMany('userVotes');
+    assert.deepEqual({
+      userVotes: suggestion.get('userVotes').map(record => record),
+      comments: suggestion.get('comments').map(record => record),
+    }, {
+      userVotes: [],
+      comments: [comment1, comment2],
+    }, `Results 'rollbackHasMany' function with 'forOnlyKey' specified as expected.`);
+
+    //Rollback all `hasMany` relationships.
+    suggestion.rollbackHasMany();
+    assert.deepEqual({
+      userVotes: suggestion.get('userVotes').map(record => record),
+      comments: suggestion.get('comments').map(record => record),
+    }, {
+      userVotes: [],
+      comments: [],
+    }, `Results 'rollbackHasMany' function without 'forOnlyKey' specified as expected.`);
+  });
+});
+
+test('rollback belongsTo relationships', function(assert) {
+  Ember.run(() => {
+    let store = this.store();
+
+    let type1 = store.createRecord('ember-flexberry-dummy-suggestion-type');
+    let type2 = store.createRecord('ember-flexberry-dummy-suggestion-type');
+
+    let user1 = store.createRecord('ember-flexberry-dummy-application-user');
+    let user2 = store.createRecord('ember-flexberry-dummy-application-user');
 
     let suggestion = store.createRecord('ember-flexberry-dummy-suggestion', {
-      address: 'address',
-      text: 'text',
-      date: new Date(),
-      votes: 0,
-      moderated: false,
       type: type1,
       author: user1,
       editor1: user1,
-    });
-
-    let vote1 = store.createRecord('ember-flexberry-dummy-vote', {
-      suggestion: suggestion,
-      voteType: 'Like',
-      applicationUser: user1,
-    });
-    let vote2 = store.createRecord('ember-flexberry-dummy-vote', {
-      suggestion: suggestion,
-      voteType: 'Dislike',
-      applicationUser: user2,
-    });
-
-    let comment1 = store.createRecord('ember-flexberry-dummy-comment', {
-      suggestion: suggestion,
-      text: 'comment1',
-      votes: 0,
-      moderated: false,
-      author: user1,
-    });
-    let comment2 = store.createRecord('ember-flexberry-dummy-comment', {
-      suggestion: suggestion,
-      text: 'comment2',
-      votes: 0,
-      moderated: false,
-      author: user2,
     });
 
     //Instead of save on server.
@@ -138,35 +119,5 @@ test('rollback relationships', function(assert) {
       author: user1,
       editor1: user1,
     }, `Results 'rollbackBelongsTo' function without 'forOnlyKey' specified as expected.`);
-
-    //Change `hasMany` relationships.
-    suggestion.set('userVotes', [vote1, vote2]);
-    suggestion.set('comments', [comment1, comment2]);
-
-    //Diff `hasMany` relationships.
-    assert.deepEqual(suggestion.changedHasMany(), {
-      userVotes: [[], [vote1, vote2]],
-      comments: [[], [comment1, comment2]],
-    }, `Results 'changedHasMany' function as expected.`);
-
-    //Rollback `hasMany` for only `userVotes` relationship.
-    suggestion.rollbackHasMany('userVotes');
-    assert.deepEqual({
-      userVotes: suggestion.get('userVotes').map(record => record),
-      comments: suggestion.get('comments').map(record => record),
-    }, {
-      userVotes: [],
-      comments: [comment1, comment2],
-    }, `Results 'rollbackHasMany' function with 'forOnlyKey' specified as expected.`);
-
-    //Rollback all `hasMany` relationships.
-    suggestion.rollbackHasMany();
-    assert.deepEqual({
-      userVotes: suggestion.get('userVotes').map(record => record),
-      comments: suggestion.get('comments').map(record => record),
-    }, {
-      userVotes: [],
-      comments: [],
-    }, `Results 'rollbackHasMany' function without 'forOnlyKey' specified as expected.`);
   });
 });
