2bc8ece45d0ca10c9ede12d468e40ab8ce3e3ce7
DrKaotica <user.email>
2016-08-10 15:51:34 +0500

    Travis fixes.

--- tests/integration/transforms/decimal-test.js
+++ tests/integration/transforms/decimal-test.js
@@ -2,124 +2,123 @@ import Ember from 'ember';
 import DS from 'ember-data';
 import { moduleFor, test } from 'ember-qunit';
 import startApp from '../../helpers/start-app';
-import QueryBuilder from 'ember-flexberry-data/query/builder';
 
 var App;
 
 moduleFor('transform:decimal', 'Integration | Transform | decimal', {
 
-    beforeEach: function () {
-        App = startApp();
-    },
+  beforeEach: function () {
+    App = startApp();
+  },
 
-    afterEach: function () {
-        Ember.$.mockjax.clear();
-    }
+  afterEach: function () {
+    Ember.$.mockjax.clear();
+  }
 
 });
 
 var TestModel = DS.Model.extend({
-    decimalNumber: DS.attr('decimal')
+  decimalNumber: DS.attr('decimal')
 });
 
 test('decimal | serialize | number', function (assert) {
-    App.register('model:testModel', TestModel);
+  App.register('model:testModel', TestModel);
 
-    let store = App.__container__.lookup('service:store');
+  let store = App.__container__.lookup('service:store');
 
-    $.mockjax({
-        url: "/test-models",
-        data: function (json) {
-            assert.equal(JSON.parse(json).DecimalNumber, 555.5);
-            return true;
-        },
-        responseText: { DecimalNumber: 555.5 }
-    });
+  $.mockjax({
+    url: "/test-models",
+    data: function (json) {
+      assert.equal(JSON.parse(json).DecimalNumber, 555.5);
+      return true;
+    },
+    responseText: { DecimalNumber: 555.5 }
+  });
 
-    Ember.run(function () {
-        let record = store.createRecord('testModel', {
-            decimalNumber: 555.5
-        }).save();
-    });
+  Ember.run(function () {
+    store.createRecord('testModel', {
+      decimalNumber: 555.5
+    }).save();
+  });
 });
 
 test('decimal | serialize | string with \'.\'', function (assert) {
-    App.register('model:testModel', TestModel);
+  App.register('model:testModel', TestModel);
 
-    let store = App.__container__.lookup('service:store');
+  let store = App.__container__.lookup('service:store');
 
-    $.mockjax({
-        url: "/test-models",
-        data: function (json) {
-            assert.equal(JSON.parse(json).DecimalNumber, 555.5);
-            return true;
-        },
-        responseText: { DecimalNumber: 555.5 }
-    });
+  $.mockjax({
+    url: '/test-models',
+    data: function (json) {
+      assert.equal(JSON.parse(json).DecimalNumber, 555.5);
+      return true;
+    },
+    responseText: { DecimalNumber: 555.5 }
+  });
 
-    Ember.run(function () {
-        let record = store.createRecord('testModel', {
-            decimalNumber: '555.5'
-        }).save();
-    });
+  Ember.run(function () {
+    store.createRecord('testModel', {
+      decimalNumber: '555.5'
+    }).save();
+  });
 
 });
 
 test('decimal | serialize | string with \',\'', function (assert) {
-    App.register('model:testModel', TestModel);
+  App.register('model:testModel', TestModel);
 
-    let store = App.__container__.lookup('service:store');
+  let store = App.__container__.lookup('service:store');
 
-    $.mockjax({
-        url: "/test-models",
-        data: function (json) {
-            assert.equal(JSON.parse(json).DecimalNumber, 555.5);
-            return true;
-        },
-        responseText: { DecimalNumber: 555.5 }
-    });
+  $.mockjax({
+    url: '/test-models',
+    data: function (json) {
+      assert.equal(JSON.parse(json).DecimalNumber, 555.5);
+      return true;
+    },
+    responseText: { DecimalNumber: 555.5 }
+  });
 
-    Ember.run(function () {
-        let record = store.createRecord('testModel', {
-            decimalNumber: '555,5'
-        }).save();
-    });
+  Ember.run(function () {
+    store.createRecord('testModel', {
+      decimalNumber: '555,5'
+    }).save();
+  });
 });
 
 test('decimal | deserialize | number', function (assert) {
-    App.register('model:testModel', TestModel);
+  App.register('model:testModel', TestModel);
 
-    let store = App.__container__.lookup('service:store');
+  let store = App.__container__.lookup('service:store');
 
-    $.mockjax({
-        url: '/test-models/1',
-        dataType: 'json',
-        responseText: { id: 1, DecimalNumber: 555.5 }
+  $.mockjax({
+    url: '/test-models/1',
+    dataType: 'json',
+    responseText: { id: 1, DecimalNumber: 555.5 }
+  });
+
+  Ember.run(function () {
+    store.findRecord('testModel', 1).then(function (data) {
+      assert.equal(data.get('decimalNumber'), 555.5);
     });
-
-    Ember.run(function () {
-        var record = store.findRecord('testModel', 1).then(function (data) {
-            assert.equal(data.get('decimalNumber'), 555.5);
-        });
-        wait();
-    });
+    wait();
+  });
 });
 
 test('decimal | deserialize | string', function (assert) {
-    App.register('model:testModel', TestModel);
+  App.register('model:testModel', TestModel);
 
-    let store = App.__container__.lookup('service:store');
+  let store = App.__container__.lookup('service:store');
 
-    $.mockjax({
-        url: '/test-models/1',
-        dataType: 'json',
-        responseText: { id: 1, DecimalNumber: '555.5' }
+  $.mockjax({
+    url: '/test-models/1',
+    dataType: 'json',
+    responseText: { id: 1, DecimalNumber: '555.5' }
+  });
+
+  Ember.run(function () {
+    store.findRecord('testModel', 1).then(function (data) {
+      assert.equal(data.get('decimalNumber'), 555.5);
     });
-
-    Ember.run(function () {
-        var record = store.findRecord('testModel', 1).then(function (data) {
-            assert.equal(data.get('decimalNumber'), 555.5);
-        });
-        wait();
-    });
+    wait();
+  });
 });
