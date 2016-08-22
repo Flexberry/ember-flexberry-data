c192908ca92d102a5cd6e19f4139df08f524ed4d
Eugene Kolegov <ekolegov@outlook.com>
2016-08-22 14:10:47 +0500

    Update audit-field model

---
+++ addon/mixins/regenerated/models/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+import { Projection } from 'ember-flexberry-data';
+
+export let Model = Ember.Mixin.create({
+  field: DS.attr('string'),
+  caption: DS.attr('string'),
+  oldValue: DS.attr('string'),
+  newValue: DS.attr('string'),
+  mainChange: DS.belongsTo('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field', { inverse: null, async: false }),
+  auditEntity: DS.belongsTo('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity', { inverse: 'auditFields', async: false }),
+  validations: {
+    auditEntity: { presence: true },
+  },
+});
+
+export let defineProjections = function (model) {
+  model.defineProjection('AuditEntityUpdateView', 'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field', {
+    field: Projection.attr(''),
+    oldValue: Projection.attr(''),
+    newValue: Projection.attr(''),
+  });
+
+  model.defineProjection('AuditFieldE', 'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field', {
+    field: Projection.attr('Поле', { hidden: true }),
+    caption: Projection.attr('Имя поля'),
+    oldValue: Projection.attr('Старое значение'),
+    newValue: Projection.attr('Новое значение'),
+    mainChange: Projection.belongsTo('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field', '', {
+    }, { hidden: true }),
+  });
+};
--- addon/models/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field.js
+++ addon/models/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field.js
@@ -1,10 +1,7 @@
-import DS from 'ember-data';
+import { Model as AuditFieldMixin, defineProjections } from '../mixins/regenerated/models/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field';
+import BaseModel from './model';
 
-export default DS.Model.extend({
-  field: DS.attr('string'),
-  caption: DS.attr('string'),
-  oldValue: DS.attr('string'),
-  newValue: DS.attr('string'),
-  mainChange: DS.belongsTo('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field', { inverse: null, async: false }),
-  auditEntity: DS.belongsTo('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity', { inverse: 'auditFields', async: false }),
-});
+let Model = BaseModel.extend(AuditFieldMixin);
+defineProjections(Model);
+
+export default Model;
