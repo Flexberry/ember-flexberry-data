c4ab3ce6c6a446c0250944b52deb11a93db3c612
Eugene Kolegov <ekolegov@outlook.com>
2016-08-22 16:22:53 +0500

    Add execution-variant enum and transform

---
+++ addon/transforms/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-t-execution-variant.js
@@ -0,0 +1,6 @@
+import FlexberryEnum from 'ember-flexberry-data/transforms/flexberry-enum';
+import tExecutionVariantEnum from '../enums/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-t-execution-variant';
+
+export default FlexberryEnum.extend({
+  enum: tExecutionVariantEnum
+});
