0e1aa11238bd57d8c461e339ab860f53b69137a5
Eugene Kolegov <ekolegov@outlook.com>
2016-08-22 19:17:30 +0500

    Fix agent model date attribute type

--- addon/mixins/regenerated/models/i-c-s-soft-s-t-o-r-m-n-e-t-security-agent.js
+++ addon/mixins/regenerated/models/i-c-s-soft-s-t-o-r-m-n-e-t-security-agent.js
@@ -18,9 +18,9 @@ export let Model = Ember.Mixin.create({
   update: DS.attr('boolean'),
   delete: DS.attr('boolean'),
   execute: DS.attr('boolean'),
-  createTime: DS.attr('string'),
+  createTime: DS.attr('date'),
   creator: DS.attr('string'),
-  editTime: DS.attr('string'),
+  editTime: DS.attr('date'),
   editor: DS.attr('string'),
   validations: {
     name: { presence: true },
