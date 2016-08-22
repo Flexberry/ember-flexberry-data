426bf5624b3b111f36d8215d34bfb17b6a2844d7
Eugene Kolegov <ekolegov@outlook.com>
2016-08-22 16:18:31 +0500

    Add link-group model

---
+++ addon/serializers/i-c-s-soft-s-t-o-r-m-n-e-t-security-link-group.js
@@ -0,0 +1,4 @@
+import { Serializer as LinkGroupSerializer } from '../mixins/regenerated/serializers/i-c-s-soft-s-t-o-r-m-n-e-t-security-link-group';
+import ODataSerializer from './odata';
+
+export default ODataSerializer.extend(LinkGroupSerializer);
