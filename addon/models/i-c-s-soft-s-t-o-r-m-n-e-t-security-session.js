0f5213a38701bf151e248a6bd8caf40e89012671
Eugene Kolegov <ekolegov@outlook.com>
2016-08-22 16:16:26 +0500

    Add session model

---
+++ addon/models/i-c-s-soft-s-t-o-r-m-n-e-t-security-session.js
@@ -0,0 +1,6 @@
+import { Model as SessionMixin } from '../mixins/regenerated/models/i-c-s-soft-s-t-o-r-m-n-e-t-security-session';
+import BaseModel from './model';
+
+let Model = BaseModel.extend(SessionMixin);
+
+export default Model;
