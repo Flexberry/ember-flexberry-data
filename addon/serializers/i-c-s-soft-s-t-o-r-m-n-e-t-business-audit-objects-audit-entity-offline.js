6a97ca3e226304f2c0b07249aa2d74103b2fd6b3
Eugene Kolegov <ekolegov@outlook.com>
2016-08-25 13:35:34 +0500

    Fix audit-entity-offline add settings for missing attributes

--- addon/serializers/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity-offline.js
+++ addon/serializers/i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity-offline.js
@@ -3,6 +3,8 @@ import { Serializer } from 'ember-flexberry-data';
 
 export default Serializer.Offline.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
+    user: { serialize: 'id', deserialize: 'records' },
+    objectType: { serialize: 'id', deserialize: 'records' },
     auditFields: { serialize: 'ids', deserialize: 'records' },
   },
 });
