42f2450bc6dae9800aba0dabd5510d906372cf0a
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-09-08 19:51:12 +0500

    Fix tests for `decimal` transform

--- tests/dummy/app/services/store.js
+++ tests/dummy/app/services/store.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 import DS from 'ember-data';
-import { Projection, Offline } from 'ember-flexberry-data';
+import { Offline } from 'ember-flexberry-data';
 
 export default Offline.Store.extend({
   init() {
     this._super(...arguments);
     let owner = Ember.getOwner(this);
     let Store = DS.Store;
-    Store.reopen(Projection.StoreMixin);
     let onlineStore = Store.create(owner.ownerInjection());
     this.set('onlineStore', onlineStore);
   }
