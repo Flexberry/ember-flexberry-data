b74b8b3a5346eb9b72a888259c22d9c04875d3fe
Eugene Kolegov <ekolegov@outlook.com>
2016-09-21 20:30:39 +0500

    Feature offline serializer convert boolean to string

--- addon/serializers/offline.js
+++ addon/serializers/offline.js
@@ -24,6 +24,48 @@ export default DS.JSONSerializer.extend({
     this.set('store', localStore);
   },
 
+  /**
+    Returns the resource's attributes formatted as a JSON-API "attributes object".
+    [More info](http://emberjs.com/api/data/classes/DS.JSONSerializer.html#method_extractAttributes).
+
+    @method extractAttributes
+    @param {DS.Model} model
+    @param {Object} hash
+    @return {Object}
+  */
+  extractAttributes(model, hash) {
+    let attributes = this._super(...arguments);
+    model.eachAttribute((key, { type }) => {
+      if (type === 'boolean') {
+        let attributeKey = this.keyForAttribute(key, 'deserialize');
+        if (typeof hash[attributeKey] === 'string') {
+          attributes[key] = hash[attributeKey] === 'true' ? true : false;
+        }
+      }
+    });
+
+    return attributes;
+  },
+
+  /**
+    Method `serializeAttribute` can be used to customize how DS.attr properties are serialized.
+    [More info](http://emberjs.com/api/data/classes/DS.JSONSerializer.html#method_serializeAttribute).
+
+    @method serializeAttribute
+    @param {DS.Snapshot} snapshot
+    @param {Object} json
+    @param {String} key
+    @param {Object} attribute
+  */
+  serializeAttribute(snapshot, json, key, attribute) {
+    let value = snapshot.attr(key);
+    if (attribute.type === 'boolean' && typeof value === 'boolean') {
+      json[key] = `${value}`;
+    } else {
+      this._super(snapshot, json, key, attribute);
+    }
+  },
+
   /*
     Returns a relationship formatted as a JSON-API "relationship object".
     See http://jsonapi.org/format/#document-resource-object-relationships
