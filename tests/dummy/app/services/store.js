37952efe4b53a791028e72ac9419864fcaf6a4f8
Eugene Kolegov <ekolegov@outlook.com>
2016-09-28 13:37:12 +0500

    Test dummy-app add offline schema for tests

--- tests/dummy/app/services/store.js
+++ tests/dummy/app/services/store.js
@@ -4,10 +4,22 @@ import { Offline } from 'ember-flexberry-data';
 
 export default Offline.Store.extend({
   init() {
-    this._super(...arguments);
+    this.set('offlineSchema', {
+      TestDB: {
+        0.1: {
+          'ember-flexberry-dummy-suggestion': 'id,address,text,date,votes,moderated,type,author,editor1,*files,*userVotes,*comments',
+          'ember-flexberry-dummy-suggestion-type': 'id,name,moderated,parent,*localizedTypes',
+          'ember-flexberry-dummy-application-user': 'id,name,eMail,phone1,phone2,phone3,activated,vK,facebook,twitter,birthday,gender,vip,karma',
+          'ember-flexberry-dummy-vote': 'id,suggestion,voteType,applicationUser',
+          'ember-flexberry-dummy-comment': 'id,suggestion,text,votes,moderated,author,*userVotes',
+          'ember-flexberry-dummy-comment-vote': 'id,comment,voteType,applicationUser',
+        }
+      },
+    });
     let owner = Ember.getOwner(this);
     let Store = DS.Store;
     let onlineStore = Store.create(owner.ownerInjection());
     this.set('onlineStore', onlineStore);
+    this._super(...arguments);
   }
 });
