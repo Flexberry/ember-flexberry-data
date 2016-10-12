8786a971d04aa192c2fd80895a9b52e7e0ae86fe
Eugene Kolegov <ekolegov@outlook.com>
2016-10-12 22:06:54 +0500

    Test adapter and offline-model and store mixins not run app

--- tests/unit/mixins/adapter-test.js
+++ tests/unit/mixins/adapter-test.js
@@ -1,23 +1,12 @@
 import Ember from 'ember';
-import AdapterMixin from 'ember-flexberry-data/mixins/adapter';
 import { module, test } from 'qunit';
+import AdapterMixin from 'ember-flexberry-data/mixins/adapter';
 
-import startApp from '../../helpers/start-app';
-
-let App;
-
-module('Unit | Mixin | adapter', {
-  setup: function () {
-    App = startApp();
-  },
-  teardown: function () {
-    Ember.run(App, 'destroy');
-  }
-});
+module('Unit | Mixin | adapter');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let AdapterObject = Ember.Object.extend(App.__container__.ownerInjection(), AdapterMixin);
+  let AdapterObject = Ember.Object.extend(AdapterMixin);
   let subject = AdapterObject.create();
   assert.ok(subject);
 });
