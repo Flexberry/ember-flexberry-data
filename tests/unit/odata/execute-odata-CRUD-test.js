1a9b3a930901a3d47f78e7f3ec8f079e2ec0b1f0
Serega Matin <seregamatin@gmail.com>
2016-10-14 19:48:48 +0500

    Fix ember exceptions in odata tests

--- tests/unit/odata/execute-odata-CRUD-test.js
+++ tests/unit/odata/execute-odata-CRUD-test.js
@@ -25,14 +25,27 @@ export default function executeTest(testName, callback) {
     const app = startApp();
     const store = app.__container__.lookup('service:store');
 
+    // Override store.unloadAll method.
+    const originalUnloadAll = store.unloadAll;
+    store.unloadAll = function() {
+      originalUnloadAll.apply(store, arguments);
+
+      // Clean up type maps otherwise internal models won't be cleaned from stores,
+      // and it will cause some exceptions related to store's internal-models statuses.
+      Ember.A([store, store.get('onlineStore'), store.get('offlineStore')]).forEach((s) => {
+        Ember.set(s, 'typeMaps', {});
+      });
+    };
+
+    // Define OData-adapter as default adapter for online store.
     const adapter = ODataAdapter.create(app.__container__.ownerInjection());
     Ember.set(adapter, 'host', baseUrl);
-
     store.get('onlineStore').reopen({
       adapterFor() {
         return adapter;
       }
     });
+
     module('OData | CRUD');
 
     test(testName, (assert) => callback(store, assert));
