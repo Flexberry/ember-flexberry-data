44f1c52509e101e90c69da138685b5a8946ec395
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-10-16 23:06:51 +0500

    Released 0.6.2-beta.23

--- addon/version.js
+++ addon/version.js
@@ -1,2 +1,2 @@
-let version = '0.6.2-beta.22';
+let version = '0.6.2-beta.23';
 export default version;
--- vendor/ember-flexberry-data/register-version.js
+++ vendor/ember-flexberry-data/register-version.js
@@ -1,3 +1,3 @@
 /* globals Ember */
-var version = '0.6.2-beta.22';
+var version = '0.6.2-beta.23';
 Ember.libraries.register('Ember Flexberry Data', version);
