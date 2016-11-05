1bb89762c5198bf34a369d80869654c8d76932f6
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-11-05 14:18:22 +0500

    Add base offline schema for tests

--- tests/unit/CRUD/offline/execute-offline-test.js
+++ tests/unit/CRUD/offline/execute-offline-test.js
@@ -36,6 +36,20 @@ export default function executeTest(testName, callback) {
       Ember.run(() => {
         db = new Dexie(dbName);
         db.version(0.1).stores({
+          'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity':
+            'id,objectPrimaryKey,operationTime,operationType,executionResult,source,serializedField,' +
+            'createTime,creator,editTime,editor,user,objectType,*auditFields',
+          'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field':
+            'id,field,caption,oldValue,newValue,mainChange,auditEntity',
+          'i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-object-type':
+            'id,name',
+          'i-c-s-soft-s-t-o-r-m-n-e-t-security-agent':
+            'id,name,login,pwd,isUser,isGroup,isRole,connString,enabled,email,full,read,insert,update,' +
+            'delete,execute,createTime,creator,editTime,editor',
+          'i-c-s-soft-s-t-o-r-m-n-e-t-security-link-group':
+            'id,createTime,creator,editTime,editor,group,user',
+          'i-c-s-soft-s-t-o-r-m-n-e-t-security-session':
+            'id,userKey,startedAt,lastAccess,closed',
           'ember-flexberry-dummy-suggestion': 'id,address,text,date,votes,moderated,type,author,editor1,*files,*userVotes,*comments',
           'ember-flexberry-dummy-suggestion-type': 'id,name,moderated,parent,*localizedTypes',
           'ember-flexberry-dummy-application-user': 'id,name,eMail,phone1,phone2,phone3,activated,vK,facebook,twitter,birthday,gender,vip,karma',
@@ -46,6 +60,12 @@ export default function executeTest(testName, callback) {
 
         db.open().then((db) => {
           let promises = [];
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-entity'));
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-audit-field'));
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-object-type'));
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-security-agent'));
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-security-link-group'));
+          promises.push(db.table('i-c-s-soft-s-t-o-r-m-n-e-t-security-session'));
           promises.push(db.table('ember-flexberry-dummy-suggestion'));
           promises.push(db.table('ember-flexberry-dummy-suggestion-type'));
           promises.push(db.table('ember-flexberry-dummy-application-user'));
