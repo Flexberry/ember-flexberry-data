cd11e80436592cd5650af519f2a56fa5156673db
Eugene Kolegov <ekolegov@outlook.com>
2016-11-08 14:43:06 +0500

    Test offline-reading-restrictions-with-odata-func-test offline not support odata function

--- tests/unit/CRUD/offline/offline-reading-restrictions-with-odata-func-test.js
+++
@@ -1,6 +0,0 @@
-import executeTest from './execute-offline-test';
-import readingRestrictionsOdataFunctions from '../base/base-reading-restrictions-with-odata-func-test';
-
-executeTest('reading | restrictions | odata functions', (store, assert) => {
-  readingRestrictionsOdataFunctions(store, assert);
-});
