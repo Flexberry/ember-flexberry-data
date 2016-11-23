f76897b0c46682d2b7cba0ad7febe7e4fbc8ff0f
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-11-23 18:46:07 +0500

    Temporarily delete `reading-data-types` test
    for Odata

--- tests/unit/CRUD/odata/odata-reading-data-types-test.js
+++
@@ -1,6 +0,0 @@
-import executeTest from './execute-odata-test';
-import readingDataTypes from '../base/base-reading-data-types-test';
-
-executeTest('reading | data types', (store, assert, App) => {
-  readingDataTypes(store, assert, App);
-});
