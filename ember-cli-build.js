a782d6eee6cbf4949f08b863cfab691964c6f60a
Eugene Kolegov <ekolegov@outlook.com>
2017-09-08 14:23:19 +0500

    :arrow_up: Update ember to 2.12.0 version

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,15 +1,8 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+/* eslint-env node */
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    jscsOptions: {
-      enabled: true,
-      esnext: true,
-      configPath: './.jscsrc'
-    }
-  });
+  var app = new EmberAddon(defaults);
 
   /*
     This build file specifies the options for the dummy test app of this
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function () {
+Router.map(function() {
   this.route('suggestion', { path: 'suggestion/:id' });
 });
 
