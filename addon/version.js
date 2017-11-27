7cd8906a45d78c87adab8bf365deb1db9ede9ac2
Evgeniy Khaberev <ehaberev@yandex.ru>
2017-11-27 17:46:31 +0500

    Released 0.9.1-beta.2

--- addon/version.js
+++ addon/version.js
@@ -1,2 +1,2 @@
-let version = '0.9.1-beta.1';
+let version = '0.9.1-beta.2';
 export default version;
--- vendor/ember-flexberry-data/register-version.js
+++ vendor/ember-flexberry-data/register-version.js
@@ -1,3 +1,3 @@
 /* globals Ember */
-var version = '0.9.1-beta.1';
+var version = '0.9.1-beta.2';
 Ember.libraries.register('Ember Flexberry Data', version);
