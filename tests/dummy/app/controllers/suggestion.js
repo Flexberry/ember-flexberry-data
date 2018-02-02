2f5b1809a0f87cb5dd8e9404f1904b02c5977d94
Semyon Volkov <volkovsema@gmail.com>
2018-02-02 12:01:25 +0500

    Fix jscs

--- tests/dummy/app/controllers/suggestion.js
+++ tests/dummy/app/controllers/suggestion.js
@@ -1,11 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  firstComment: Ember.computed('model.comments.[]', function() {
-    return this.get('model.comments.firstObject');
-  }),
+  hello: 'sup',
+  owner: null,
 
-  firstCommentVote: Ember.computed('firstComment.userVotes.[]', function() {
-    return this.get('firstComment.userVotes.firstObject');
-  })
+  init() {
+    let owner = Ember.getOwner(this);
+    this.set('owner', owner);
+    let a = owner.lookup('adapter:odata');
+    console.log(owner);
+    console.log(a);
+
+    // successCallback, failCallback, alwaysCallback
+    console.log(a.callFunction(
+      'http://localhost:8600/Odata',
+      'ValidateCfaptcha',
+      { clientResponse: 'dfsdf',
+        meme: 'mems',
+        count: 4 }
+    ));
+    console.log(a.callAction(
+      'http://localhost:8600/Odata',
+      'ValidateUsernafme',
+      { username: 'dfsdf',
+        ping: 'pong',
+        count: 5 }
+    ));
+  }
 });
