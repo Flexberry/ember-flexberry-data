e4a2efe27c13c605a57b9095e9b7ddeb145bcd79
Evgeniy Khaberev <ehaberev@yandex.ru>
2018-04-18 13:49:30 +0500

    Fix `yarn lint:js` command running

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'ember-flexberry-data',
 
   included: function(app) {
-    this._super(...arguments);
+    this._super.apply(this._super, arguments);
 
     app.import('vendor/ember-flexberry-data/register-version.js');
   }
