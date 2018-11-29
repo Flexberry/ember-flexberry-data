b1eb7aeba15f54fb04c36065ae7e67368efaf466
Serega Matin <seregamatin@gmail.com>
2018-11-29 11:53:38 +0500

    Update dependencies list in default blueprint

--- blueprints/ember-flexberry-data/index.js
+++ blueprints/ember-flexberry-data/index.js
@@ -4,12 +4,14 @@ module.exports = {
     var _this = this;
     return _this.addAddonsToProject({
       packages: [
-        { name: 'ember-moment', target: '7.6.0' }
+        { name: 'ember-auto-import': '1.2.16' },
+        { name: 'ember-data': '3.5.0' },
+        { name: 'ember-moment', target: '7.8.0' }
       ]
     }).then(function () {
       return _this.addPackagesToProject([
         { name: 'dexie', target: '2.0.2' },
-        { name: 'node-uuid', target: '1.4.7' }
+        { name: 'node-uuid', target: '1.4.8' }
       ]);
     });
   },
