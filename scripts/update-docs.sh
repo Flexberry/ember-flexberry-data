59c6aa02d4f5043d10b6255178452a021ed0760d
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-02-11 11:29:59 +0500

    Add script for deploy autodoc

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -1,30 +1,65 @@
-#!/bin/bash
-
 # Exit with nonzero exit code if anything fails.
 set -e
 
+# Define repository relative GitHub address.
+repositoryRelativeGitHubAddress="Flexberry/ember-flexberry-data"
+
+# Clone project into 'repository' subdirectory && move to it.
+echo "Prepare for deploy to gh-pages."
+echo "Clone ${repositoryRelativeGitHubAddress} repository & checkout latest version of gh-pages branch."
+git clone --recursive "https://github.com/${repositoryRelativeGitHubAddress}.git" emberFlexberryDataRepository
+cd emberFlexberryDataRepository
+
+# Checkout gh-pages branch & pull it's latest version.
+git checkout gh-pages
+git pull
+
+cd ..
+
+# Generate autodoc.
 npm install -g yuidocjs
-git clone --recursive https://github.com/Flexberry/flexberry.github.io.git docs
-cd docs
 
-npm install
-git submodule update --remote --merge
-cd "${TRAVIS_BRANCH}"
-rm -rf assets
-rm -rf classes
-rm -rf elements
-rm -rf files
-rm -rf modules
+# Define yuidoc theme repository relative GitHub address.
+repositoryYuidocTheme="Flexberry/flexberry-yuidoc-theme"
 
+# Clone project into 'repositoryYuidocTheme' subdirectory && move to it.
+echo "Prepare for deploy to gh-pages."
+echo "Clone ${repositoryYuidocTheme} repository & checkout latest version of gh-pages branch."
+git clone --recursive "https://github.com/${repositoryYuidocTheme}.git" repositoryYuidocTheme
+cd repositoryYuidocTheme
+
+# Checkout and pull same branch.
+git checkout ${TRAVIS_BRANCH}
+git pull
+
+echo "Copy ember addon source (for ${TRAVIS_BRANCH} branch) into addon directory."
+mkdir addon
+cp -r ../addon/* addon
+
+echo "Execute yuidoc autodocumentation generator."
 yuidoc
 
+echo "Navigate to target directory for autodoc in gh-pages."
+cd "../emberFlexberryDataRepository/autodoc"
+
+# Remove results of previous deploy (for current branch) & recreate directory.
+rm -rf "${TRAVIS_BRANCH}"
+mkdir "${TRAVIS_BRANCH}"
+
+echo "Copy autodoc result into ${TRAVIS_BRANCH} directory."
+cp -r ../../repositoryYuidocTheme/autodoc-result/* ${TRAVIS_BRANCH}
+
+cd ..
+
+# Configure git.
 git config user.name "Flexberry-man"
 git config user.email "mail@flexberry.net"
 
+echo "Commit & push changes."
 git add --all
-git commit -m "Update docs" -m "By Travis, Repo ${TRAVIS_REPO_SLUG}, Build #${TRAVIS_BUILD_NUMBER}, Commit ${TRAVIS_COMMIT}, Branch ${TRAVIS_BRANCH}."
+git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
 
 # Redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/Flexberry/flexberry.github.io.git" > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryRelativeGitHubAddress}.git" > /dev/null 2>&1
 
-echo "Done."
+echo "Deploy to gh-pages finished."
