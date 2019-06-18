0e56650253a23df6dc0e643fa83488925de64b3c
Bratchikov Igor <bratchikov@ics.perm.ru>
2019-06-18 16:31:47 +0500

    Simplify test data for batch request testing

--- tests/unit/CRUD/base/base-batch-updating-test.js
+++ tests/unit/CRUD/base/base-batch-updating-test.js
@@ -85,57 +85,11 @@ function initTestData(store) {
           parent: parentType
         }).save()
       ])
-    )
-
-    // Сreating suggestion.
-    .then((sugAttrs) =>
-      store.createRecord('ember-flexberry-dummy-suggestion', {
-        type: sugAttrs[3],
-        author: sugAttrs[0],
-        editor1: sugAttrs[1]
-      }).save()
-
-        // Creating comment.
-        .then((sug) =>
-          Ember.RSVP.Promise.all([
-            store.createRecord('ember-flexberry-dummy-comment', {
-              author: sugAttrs[0],
-              text: 'Comment 1',
-              suggestion: sug,
-            }).save(),
-
-            store.createRecord('ember-flexberry-dummy-comment', {
-              author: sugAttrs[0],
-              text: 'Comment 2',
-              suggestion: sug,
-            }).save()
-          ])
-
-            // Creating votes.
-            .then((comments) =>
-              Ember.RSVP.Promise.all([
-                store.createRecord('ember-flexberry-dummy-comment-vote', {
-                  applicationUser: sugAttrs[1],
-                  comment: comments[0]
-                }).save(),
-
-                store.createRecord('ember-flexberry-dummy-comment-vote', {
-                  applicationUser: sugAttrs[2],
-                  comment: comments[1]
-                }).save(),
-              ])
-
-                .then((votes) =>
-                  new Ember.RSVP.Promise((resolve) =>
-                    resolve({
-                      people: sugAttrs.slice(0, 3).map(item => item.get('id')),
-                      comments: comments.map(item => item.get('id')),
-                      votes: votes.map(item => item.get('id')),
-                      sug: sug.get('id')
-                    })
-                  )
-                )
-            )
-        )
+    ).then((createdRecords) =>
+      new Ember.RSVP.Promise((resolve) =>
+        resolve({
+          people: createdRecords.slice(0, 3).map(item => item.get('id'))
+        })
+      )
     );
 }
