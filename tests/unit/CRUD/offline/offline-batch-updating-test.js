940494a6049c6881167fa10eeb126388216509f0
Eugene Kolegov <ekolegov@outlook.com>
2019-06-28 10:13:53 +0500

    Delete test offline batch updating

--- tests/unit/CRUD/offline/offline-batch-updating-test.js
+++
@@ -1,6 +0,0 @@
-import executeTest from './execute-offline-test';
-import batchUpdating from '../base/base-batch-updating-test';
-
-executeTest('batchUpdating', (store, assert) => {
-  batchUpdating(store, assert);
-});
