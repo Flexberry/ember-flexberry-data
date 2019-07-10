5d50c4d46c6e4cdc5b793b0bc8a3caf311e9073b
Flexberry-man <mail@flexberry.net>
2019-07-10 09:41:55 +0000

    Update gh-pages for develop branch

--- autodoc/develop/api.js
+++ autodoc/develop/api.js
@@ -6,6 +6,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "Adapter.Offline",
         "ApplicationInitializer",
         "Audit",
+        "CopyableMixin",
         "DecimalTransform",
         "FileTransform",
         "FlexberryEnumTransform",
