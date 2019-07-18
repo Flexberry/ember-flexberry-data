9e913f6af9ac54f59aa42702913419c0ad576adf
Flexberry-man <mail@flexberry.net>
2019-07-18 05:54:42 +0000

    Update gh-pages for feature-ember-update branch

---
+++ autodoc/feature-ember-update/api.js
@@ -0,0 +1,65 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AdapterMixin",
+        "ApplicationInitializer",
+        "Base",
+        "BaseAdapter",
+        "BaseBuilder",
+        "BasePredicate",
+        "Builder",
+        "ComplexPredicate",
+        "Condition",
+        "CopyableMixin",
+        "DatePredicate",
+        "DecimalTransform",
+        "DexieService",
+        "FileTransform",
+        "FilterOperator",
+        "FlexberryEnumTransform",
+        "GlobalsService",
+        "GuidTransform",
+        "IndexedDBAdapter",
+        "Information",
+        "JsAdapter",
+        "LocalStore",
+        "Model",
+        "ModelMixin",
+        "OData",
+        "OData.OnlineStore",
+        "ODataAdapter",
+        "Offline",
+        "OrderByClause",
+        "Projection",
+        "Query.Condition",
+        "Query.DetailPredicate",
+        "Query.FilterOperator",
+        "Query.GeographyPredicate",
+        "Query.GeometryPredicate",
+        "Query.IsOfPredicate",
+        "Query.NotPredicate",
+        "Query.OrderByClause",
+        "QueryObject",
+        "SimplePredicate",
+        "Store",
+        "StoreMixin",
+        "StringPredicate",
+        "Syncer"
+    ],
+    "modules": [
+        "ember-flexberry",
+        "ember-flexberry-data"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-flexberry",
+            "name": "ember-flexberry"
+        },
+        {
+            "displayName": "ember-flexberry-data",
+            "name": "ember-flexberry-data"
+        }
+    ],
+    "elements": []
+} };
+});
