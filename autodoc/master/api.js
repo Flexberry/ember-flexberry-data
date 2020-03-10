9627d6560a4204fa01f2bd04a1ddf9eb9eb3d49a
Flexberry-man <mail@flexberry.net>
2020-03-10 09:52:24 +0000

    Update gh-pages for master branch

---
+++ autodoc/master/api.js
@@ -0,0 +1,73 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Adapter",
+        "Adapter.OData",
+        "Adapter.Offline",
+        "ApplicationInitializer",
+        "Audit",
+        "CopyableMixin",
+        "DecimalTransform",
+        "FileTransform",
+        "FlexberryEnumTransform",
+        "GuidTransform",
+        "OData.OnlineStore",
+        "Offline",
+        "Offline.DexieService",
+        "Offline.GlobalsService",
+        "Offline.LocalStore",
+        "Offline.Model",
+        "Offline.ModelMixin",
+        "Offline.Store",
+        "Offline.Syncer",
+        "Projection",
+        "Projection.AdapterMixin",
+        "Projection.Model",
+        "Projection.StoreMixin",
+        "Query",
+        "Query.BaseAdapter",
+        "Query.BaseBuilder",
+        "Query.BasePredicate",
+        "Query.Builder",
+        "Query.ComplexPredicate",
+        "Query.Condition",
+        "Query.DatePredicate",
+        "Query.DetailPredicate",
+        "Query.FilterOperator",
+        "Query.GeographyPredicate",
+        "Query.GeometryPredicate",
+        "Query.IndexedDBAdapter",
+        "Query.IsOfPredicate",
+        "Query.JsAdapter",
+        "Query.NotPredicate",
+        "Query.ODataAdapter",
+        "Query.OrderByClause",
+        "Query.QueryObject",
+        "Query.SimplePredicate",
+        "Query.StringPredicate",
+        "Security",
+        "Serializer",
+        "Serializer.Base",
+        "Serializer.OData",
+        "Serializer.Offline",
+        "Utils",
+        "Utils.Information"
+    ],
+    "modules": [
+        "ember-flexberry",
+        "ember-flexberry-data"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-flexberry",
+            "name": "ember-flexberry"
+        },
+        {
+            "displayName": "ember-flexberry-data",
+            "name": "ember-flexberry-data",
+            "description": "Ember Addon that adds support of projections in models."
+        }
+    ],
+    "elements": []
+} };
+});
