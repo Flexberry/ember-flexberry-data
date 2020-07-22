f97d5aae13f1191695fcaa472b6e80c9c24f2925
leoleopon <safari2002@list.ru>
2020-07-22 16:47:49 +0500

    Sync with PR #169 by test store definition.

--- tests/unit/CRUD/odata/execute-odata-test.js
+++ tests/unit/CRUD/odata/execute-odata-test.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 import { module, test } from 'qunit';
+import OnlineStore from 'ember-flexberry-data/stores/online-store';
 
 import { Adapter, Projection } from 'ember-flexberry-data';
 
@@ -18,7 +18,7 @@ export default function executeTest(testName, callback) {
 
     const app = startApp();
     const store = app.__container__.lookup('service:store');
-    let onlineStore = DS.Store.reopen(Projection.StoreMixin).create(app.__container__.ownerInjection());
+    let onlineStore = OnlineStore.reopen(Projection.StoreMixin).create(app.__container__.ownerInjection());
     store.set('onlineStore', onlineStore);
 
     // Override store.unloadAll method.
@@ -36,6 +36,7 @@ export default function executeTest(testName, callback) {
     // Define OData-adapter as default adapter for online store.
     const adapter = Adapter.Odata.create(app.__container__.ownerInjection());
     Ember.set(adapter, 'host', baseUrl);
+    Ember.set(adapter, 'store', store);
     store.get('onlineStore').reopen({
       adapterFor() {
 
