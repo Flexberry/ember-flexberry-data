dbf5ef725b2be95adf276b0d450431c1f6ab3bf5
leoleopon <safari2002@list.ru>
2020-07-27 11:46:49 +0500

    Docker test backend URL is specified.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -51,8 +51,9 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
 
-    // TODO: find proper way to pass arguments to tests
-    var testODataServiceURL = process.env.TEST_ODATA_SERVICE_URL;
+    // URL of the backend running in docker
+    var testODataServiceURL = 'http://localhost:6500';
+
     ENV.APP.testODataService = !!testODataServiceURL;
     ENV.APP.testODataServiceURL = testODataServiceURL;
   }
