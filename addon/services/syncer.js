708d0d58c2c311c40bb709a862f016c088f3f2f3
leoleopon <safari2002@list.ru>
2020-07-29 16:38:00 +0500

    Fix camelize() and capitalize() usage in syncer.

--- addon/services/syncer.js
+++ addon/services/syncer.js
@@ -4,6 +4,7 @@ import { SimplePredicate } from '../query/predicate';
 import { reloadLocalRecords, createLocalRecord } from '../utils/reload-local-records';
 import isModelInstance from '../utils/is-model-instance';
 import Queue from '../utils/queue';
+import { camelize, capitalize } from '../utils/string-functions';
 
 const { RSVP, isNone, isArray, getOwner, get } = Ember;
 
@@ -326,7 +327,7 @@ export default Ember.Service.extend({
     const modelClass = store.modelFor(modelName);
 
     let projectionName = modelName.indexOf('-') > -1 ? modelName.substring(modelName.indexOf('-') + 1) : modelName;
-    projectionName = projectionName.camelize().capitalize() + 'E';
+    projectionName = capitalize(camelize(projectionName)) + 'E';
 
     if (modelClass.projections) {
       if (modelClass.projections.get(projectionName)) {
