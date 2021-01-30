2c5bd223798b2deb3bd61dee6a5a2ab83c503b25
Eugene Kolegov <ekolegov@outlook.com>
2021-01-30 18:08:01 +0500

    Ignore all current linting errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,5 +4,20 @@ module.exports = {
   extends: ['recommended', 'stylistic'],
   rules: {
     'no-bare-strings': true
-  }
+  },
+  pending: [
+    {
+      "moduleId": "tests/dummy/app/templates/application",
+      "only": [
+        "eol-last",
+        "no-bare-strings"
+      ]
+    },
+    {
+      "moduleId": "tests/dummy/app/templates/suggestion",
+      "only": [
+        "no-bare-strings"
+      ]
+    }
+  ]
 };
