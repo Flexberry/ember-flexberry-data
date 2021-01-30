81eaf100292f06786ea28a51c72261a692732c6a
Eugene Kolegov <ekolegov@outlook.com>
2021-01-30 16:25:28 +0500

    Uninstall broccoli-jscs package with JSCS linting

--- vendor/flexberry/ember-cli-build.js
+++ vendor/flexberry/ember-cli-build.js
@@ -5,11 +5,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    jscsOptions: {
-      enabled: true,
-      esnext: true,
-      configPath: './.jscsrc'
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
