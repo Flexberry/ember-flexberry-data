b84075f1262d98ee1c8073910d9887bebf0bc0ff
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2022-12-02 15:58:34 +0500

    Changed backend url

--- tests/unit/CRUD/odata/execute-odata-test.js
+++ tests/unit/CRUD/odata/execute-odata-test.js
@@ -18,7 +18,7 @@ export default function executeTest(testName, callback, skipTest) {
     if (config.APP.testODataServiceURL.indexOf('http') >= 0) {
       baseUrl = config.APP.testODataServiceURL;
     } else {
-      baseUrl = 'http://localhost:6500/odata';
+      baseUrl = 'http://localhost:80/odata';
     }
 
     const app = startApp();
