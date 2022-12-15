9478cb57b4674bb3f14c08073aad00a3d85c06ac
Eugene Kolegov <KollegOFF@users.noreply.github.com>
2022-12-15 13:33:29 +0500

    Use jquery-mockjax from npm package instead of bower (#203)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,15 @@
-/* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     'ember-cli-qunit': {
       useLintTree: false
+    },
+
+    nodeAssets: {
+      'jquery-mockjax': {
+        import: ['src/jquery.mockjax.js']
+      }
     }
   });
 
@@ -14,10 +19,6 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
-  if (app.env !== 'production') {
-    app.import('bower_components/jquery-mockjax/dist/jquery.mockjax.js');
-  }
 
   return app.toTree();
 };
