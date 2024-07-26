217dc792fca96266e41113bed8bbd6619ef526a8
DubrovinPavel <dubrovip@yandex.ru>
2024-07-26 09:50:17 +0500

    Fix addon blueprint

--- blueprints/ember-flexberry-data/index.js
+++ blueprints/ember-flexberry-data/index.js
@@ -1,18 +1,11 @@
 /* globals module */
 module.exports = {
   afterInstall: function() {
-    let _this = this;
-    return _this.addAddonsToProject({
-      packages: [
-        { name: 'ember-browserify', target: '1.1.9' }
-      ]
-    }).then(function () {
-      return _this.addPackagesToProject([
-        { name: 'dexie', target: '2.0.2' },
-        { name: 'node-uuid', target: '1.4.7' },
-        { name: 'ember-moment', target: '7.6.0' }
-      ]);
-    });
+    return this.addPackagesToProject([
+      { name: 'dexie', target: '2.0.2' },
+      { name: 'ember-uuid', target: '2.0.0' },
+      { name: 'ember-moment', target: '^9.0.1' }
+    ]);
   },
 
   normalizeEntityName: function() {}
